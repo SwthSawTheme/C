7d65d771a3379e0bb6316b49c397fa7ab7638110
SwthSawTheme <sawtheme1@gmail.com>
2024-06-29 16:55:42 -0300

    teste

---
+++ raylibCreatives/nom.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    
+    char name[1];
+
+    printf("Digite seu nome: ");
+    scanf("%s",&name);
+
+    printf("%s",name);
+
+    return 0;
+}
