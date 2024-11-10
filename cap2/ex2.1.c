93db42183cdba981850b023ac5421035eab04c99
SwthSawTheme <sawtheme1@gmail.com>
2024-11-10 15:35:14 -0300

    exercicio capitulo 2.1 resolvido

---
+++ cap2/ex2.1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( void ){
+    
+    int numero;
+
+    printf("Digite um numero: ");
+    scanf("%d",&numero);
+
+    if (numero % 2 == 0){
+        printf("O numero e par!");
+
+    } else {
+        printf("O numero e impar!");
+    }
+
+    return 0;
+}
