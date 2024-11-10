feb8d14e4b0271f9483ad3a313a258c540b597cf
SwthSawTheme <sawtheme1@gmail.com>
2024-11-10 16:07:19 -0300

    ex2.3 resolvido

---
+++ cap2/ex2.3.c
@@ -0,0 +1,28 @@
+// Exercício 2.3:
+// Escreva um programa que peça para o usuário fornecer dois números inteiros. O programa deve
+// exibir esses dois números em ordem decrescente.
+// Arquivo com a solução: ex2.3.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main( void ){
+    
+    int numero1;
+    int numero2;
+
+    printf("Entre com um numero: ");
+    scanf("%d",&numero1);
+
+    printf("Entre com outro numero: ");
+    scanf("%d",&numero2);
+
+    if (numero1 >= numero2) {
+        printf("Ordem decrescente: %d >= %d",numero1,numero2);
+    } else {
+        printf("Ordem decrescente: %d >= %d",numero2,numero1);
+    }
+
+    return 0;
+}
